3b50de72e95f490601cb1aea1c11a8222a719b9a
Acet-Aminophen <leeds06080@gmail.com>
2022-09-05 10:45:36 +0900

    ADD : there are two output files, summary and result ADD : output file's name is changed to uuid ADD : now you can designate output files location

--- encryptor.py
+++ encryptor.py
@@ -1,6 +1,7 @@
 import hashlib
 import random
 import sys
+import uuid
 
 
 def sha256(str1: str):
@@ -16,24 +17,34 @@ def salt(org_str: str, salt_str: str, unique_number_range: int):
     return salted
 
 
-def encrypt(file_location: str, salt_str: str, unique_number_range: int, iteration: int):
+def encrypt(file_location: str, salt_str: str, unique_number_range: int, iteration: int, output_location: str):
     with open(file_location, "r", encoding="utf-8") as f:
         # 개행 문자 제거를 위한 splitlines() 사용
         line_list = f.read().splitlines()
-        output = []
+        result_output = []
         for i in line_list:
             if i.strip() == "":
                 continue
             encryption_target = salt(i, salt_str, unique_number_range)
             for j in range(iteration):
                 encryption_target = sha256(encryption_target)
-            output.append(encryption_target)
-        with open("output.txt", "w") as outfile:
-            outfile.write("\n".join(output))
+            result_output.append(encryption_target)
+        uuid_str = str(uuid.uuid4())
+
+        summary_file_location = output_location + "Summary_" + uuid_str + ".txt"
+        result_file_location = output_location + "Result_" + uuid_str + ".txt"
+
+        with open(summary_file_location, "w") as outfile:
+            summary_output = ["FILE_LOCATION : " + file_location, "SALT : " + salt_str,
+                              "UNIQUE_NUMBER_RANGE : " + str(unique_number_range), "ITERATION : " + str(iteration)]
+            outfile.write("\n".join(summary_output))
+
+        with open(result_file_location, "w") as outfile:
+            outfile.write("\n".join(result_output))
 
 
 if __name__ == '__main__':
-    encrypt(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+    encrypt(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]), sys.argv[5] + "\\" if len(sys.argv) > 5 else "")
 
 # TDL
 # 줄(Line)의 개수가 노출되는 것이 핵심 -> 개별 Line의 암호화를 통해 변화율 측정 가능, 다만 Line을 섞는 것은 가능할 듯
